d21c94f7cde686de03693fe919410866933a2f60
Austin <austincumberlander@gmail.com>
2019-04-15 09:15:10 -0500

    completed FizzBuzz challenge in C#

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FizzBuzz
 {
@@ -6,7 +8,39 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Enumerable.Range(0, 106).ToList();
+
+            //List<int> numbers = new List<int>() { 1, 2, (-3), 4, (-5), 15 };
+
+            void FizzBuzz(int divByThree, int divByFive, int newMultiple, string newWord)
+            {
+
+                foreach(int number in numbers)
+                {
+                    string message = "";
+
+                    if (number == 0)
+                    {
+                        Console.WriteLine($" {number}");
+                        continue;
+                    }
+                    if (number % divByThree == 0)
+                    {
+                        message += "Fizz";
+                    }
+                    if (number % divByFive == 0)
+                    {
+                        message += "Buzz";
+                    }
+                    if (number % newMultiple == 0)
+                    {
+                        message += $"{newWord}";
+                    }
+                    Console.WriteLine($"{message} {number}");
+                }
+                Console.ReadLine();
+            }
+            FizzBuzz(3, 5, 7, "Jazz");
         }
     }
 }
